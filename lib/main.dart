7445267746efccd05738b476853195a398d7a36d
dawirashid <77828860+dawirashid@users.noreply.github.com>
2022-01-13 12:07:01 +0800

    add sign in google done

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      
       home: GoogleSignInApp(),
     );
   }
@@ -25,8 +25,44 @@ class GoogleSignInApp extends StatefulWidget {
 }
 
 class _GoogleSignInAppState extends State<GoogleSignInApp> {
+  // 1 Initialize google sign in
+  final GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['email']);
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    // 2 add User var
+    GoogleSignInAccount? user = _googleSignIn.currentUser;
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          //3 check if user is null otherwise user is sign in
+          title: Text(
+              'Google Demo (Logged ' + (user == null ? 'out' : 'in') + ')'),
+        ),
+        body: Center(
+          child: Column(
+            children: [
+              // 4 ADD elevated button and setState
+              ElevatedButton(
+                // if the user sign in disable the sign in button
+                onPressed:  user != null ? null : () async {
+                  await _googleSignIn.signIn();
+                  setState(() {});
+                },
+                child: const Text('Sign In'),
+              ),
+              ElevatedButton(
+                // if the user sign out disable the sign out button
+                onPressed: user  == null ? null : () async {
+                  await _googleSignIn.signOut();
+                  setState(() {});
+                },
+                child: const Text('Sign Out'),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
